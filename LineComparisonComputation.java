757ca4fc7146e869c9a1d26ce19c510b6c1a2e80
aytech11 <aytech0110@gmail.com>
2022-08-26 16:31:01 +0530

    Line Comparison Computation Program

---
+++ LineComparisonComputation.java
@@ -0,0 +1,13 @@
+package day3practiceproblemsPart2;
+
+public class LineComparisonComputation {
+
+	
+	public static void main(String[] args) {
+	
+	System.out.println("Welcome to Line Comparison Computation Program");
+	
+	}
+	
+	
+}
