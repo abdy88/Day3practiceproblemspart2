375235fa1535e39f14996be2170d45e4d0f858c4
aytech11 <aytech0110@gmail.com>
2022-08-27 17:42:52 +0530

    UC2 check equality of two lines

--- LineComparisonComputation.java
+++ LineComparisonComputation.java
@@ -4,12 +4,77 @@ import java.util.Scanner;
 
 public class LineComparisonComputation {
 
+	static double linelength1;
+	static double linelength2;
+
 	
 	public static void main(String[] args) {
 	
 	System.out.println("Welcome to Line Comparison Computation Program");
-	
+	linelength1=calcLinelength();
+	System.out.println("Enter coordinates of second line");
+	linelength2=calcLinelength();
+	checkEqualityOfTwoLines();
+
 	}
+	
+	
+	 static double calcLinelength() {
+		 
+		 	Scanner myObj = new Scanner(System.in);
+			
+
+		 	System.out.println("Enter x1 coordinate ");
+			float x1 = myObj.nextFloat();
+			System.out.println("Enter y1 coordinate ");
+			float y1 = myObj.nextFloat();
+			System.out.println("Enter x2 coordinate ");
+			float x2 = myObj.nextFloat();
+			System.out.println("Enter y2 coordinate ");
+			float y2 = myObj.nextFloat();
+			
+			
+			System.out.println(" x1= " + x1 );
+			System.out.println(" y1= " + y1 );
+			System.out.println(" x2= " + x2 );
+			System.out.println(" y2= " + y2 );
+		 
+			double linelength = (Math.sqrt(( ((x2 - x1) *(x2 - x1)) + ((y2- y1) * (y2 - y1)))));
+			System.out.println("line length is = " + linelength  );
+			
+			return linelength;
+			
+			
+		  }
+
+	
+	 static void checkEqualityOfTwoLines()
+   	{
+     	
+
+		 	if (linelength1==linelength2)
+		 		{
+		 			System.out.println("Two lines are equal");
+ 		  
+		 		}
+		  
+		 	else
+		 	{
+		 			System.out.println("Two lines are not equal");
+ 		  
+ 		  
+		 	}
+			
+ 	 			
+   }
+   
+
+	
+	
+	
+	
+	
+	
 	
 	
 }
